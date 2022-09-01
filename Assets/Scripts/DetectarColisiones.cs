cafcab70468f2fd5f0c1f523bf1c05ae3998767d
jsanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-09-01 17:58:32 -0300

    Terminado por ahora

---
+++ Assets/Scripts/DetectarColisiones.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectarColisiones : MonoBehaviour
+{
+    public Text txtPuntaje;
+    public int puntaje;
+    // Start is called before the first frame update
+    void Start()
+    {
+        puntaje = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txtPuntaje.text = puntaje.ToString();
+    }
+
+    void OnCollisionEnter2D()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D()
+    {
+        puntaje++;
+    }
+    
+}
---
+++ Assets/Scripts/EstablecerPosicionAlAzar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstablecerPosicionAlAzar : MonoBehaviour
+{
+    public float alturaMinima;
+    public float alturaMaxima;
+    public float nroAlAzar;
+    public Vector3 posicionInicial;
+    public float limiteObstaculo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerarNroAlAzar();
+        transform.position += Vector3.up * nroAlAzar;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < limiteObstaculo)
+        {
+            GenerarNroAlAzar();
+            transform.position = posicionInicial + Vector3.up * nroAlAzar;
+        }
+    }
+
+    void GenerarNroAlAzar()
+    {
+        nroAlAzar = Random.Range(alturaMinima, alturaMaxima);
+    }
+}
