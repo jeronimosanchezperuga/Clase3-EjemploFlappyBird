9ba7b8784a62643a84efbc9f03b7aa790ec0620f
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-09-01 15:26:57 -0300

    FondoSeDesplaza y repite

---
+++ Assets/Scripts/ReposicionarAlLlegarAValorLimite.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReposicionarAlLlegarAValorLimite : MonoBehaviour
+{
+    public Vector3 posicionInicial;
+    public float valorLimite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionInicial = transform.position;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x <= valorLimite)
+        {
+            transform.position = posicionInicial;
+        }
+    }
+}
